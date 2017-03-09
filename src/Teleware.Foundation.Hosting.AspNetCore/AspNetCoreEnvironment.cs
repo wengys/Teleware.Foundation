bc2052a17b9bada1dac3d2067cbf19870996b0b9
wengys <wengys@teleware.cn>
2017-03-09 16:30:23 +0800

    增加AspNetCore环境

---
+++ src/Teleware.Foundation.Hosting.AspNetCore/AspNetCoreEnvironment.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using Teleware.Foundation.Hosting;
+
+namespace Teleware.Foundation.Host.AspNetCore
+{
+    /// <summary>
+    /// AspNetCore执行环境
+    /// </summary>
+    public class AspNetCoreEnvironment : IEnvironment
+    {
+        private readonly IHostingEnvironment _env;
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="environmentName">环境名</param>
+        /// <param name="contentRootPath">资源根路径</param>
+        public AspNetCoreEnvironment(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
+        /// <summary>
+        /// 当前环境名
+        /// </summary>
+        public string EnvironmentName => _env.EnvironmentName;
+
+        /// <summary>
+        /// 资源根路径
+        /// </summary>
+        public string ContentRootPath => _env.ContentRootPath;
+    }
+}
---
+++ src/Teleware.Foundation.Hosting.AspNetCore/Module.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+
+namespace Teleware.Foundation.Host.Application
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public class Module : Autofac.Module
+    {
+        /// <inheritdoc/>
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<AspNetCoreEnvironment>().As<IEnvironment>().SingleInstance();
+        }
+    }
+}
