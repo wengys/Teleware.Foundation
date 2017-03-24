917d499825eba410a0238dc5b612c739f5ad6e13
wengys <wengys@teleware.cn>
2017-03-24 14:07:28 +0800

    增加webapi相关拦截器

---
+++ src/Teleware.Foundation.AspNet.WebApi.Filters/EventIds.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teleware.Foundation.AspNet.WebApi.Filters
+{
+    /// <summary>
+    /// 事件Id
+    /// </summary>
+    public static class EventIds
+    {
+        public const int ClientNoticeableExceptionEventId = 1000;
+        public const int HttpClientNoticeableExceptionEventId = 1050;
+        public const int UnknownExceptionEventId = 2000;
+    }
+}
